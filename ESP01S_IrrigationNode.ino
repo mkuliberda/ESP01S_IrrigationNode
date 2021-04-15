1af53668c412fd3c3d5d07dc60231d4a8f673ded
mkuliberda <m.kuliberda@s4t.com.pl>
2021-04-15 21:44:50 +0200

    minor changes an cleanups

--- ESP01S_IrrigationNode.ino
+++ ESP01S_IrrigationNode.ino
@@ -23,8 +23,6 @@ void setup() {
   Serial.begin(115200);
   configTime(MY_TZ, MY_NTP_SERVER); // --> Here is the IMPORTANT ONE LINER needed in your sketch!
 
-  //Serial.println();
-
   // We start by connecting to a WiFi network
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, pass);
@@ -33,16 +31,16 @@ void setup() {
     delay(500);
     //Serial.print(".");
   }
-  //Serial.println("");
   delay(5000);
   getTime();
+  delay(1000);
 }
 
 void loop() {
   //handleCommunication();
   //todo: msgs broker
-  delay(1000);
   getTime();
+  delay(1000);
 }
 
 void handleCommunication(){
@@ -101,34 +99,6 @@ void getTime(){
   obj["CTD"] = buf;
   serializeJson(doc, Serial);
   Serial.println();
-  Serial.println(buf);
-  //doc.clear();
+  //Serial.println(buf);
   
 }
-
-/*void showTime() {
-  time(&now);                       // read the current time
-  localtime_r(&now, &tm);           // update the structure tm with the current time
-  Serial.print("year:");
-  Serial.print(tm.tm_year + 1900);  // years since 1900
-  Serial.print("\tmonth:");
-  Serial.print(tm.tm_mon + 1);      // January = 0 (!)
-  Serial.print("\tday:");
-  Serial.print(tm.tm_mday);         // day of month
-  Serial.print("\thour:");
-  Serial.print(tm.tm_hour);         // hours since midnight  0-23
-  Serial.print("\tmin:");
-  Serial.print(tm.tm_min);          // minutes after the hour  0-59
-  Serial.print("\tsec:");
-  Serial.print(tm.tm_sec);          // seconds after the minute  0-61*
-  Serial.print("\twday");
-  Serial.print(tm.tm_wday);         // days since Sunday 0-6
-  if (tm.tm_isdst == 1)             // Daylight Saving Time flag
-    Serial.print("\tDST");
-  else
-    Serial.print("\tstandard");
-  Serial.println();
-
-  delay(1000);
-
-}*/
